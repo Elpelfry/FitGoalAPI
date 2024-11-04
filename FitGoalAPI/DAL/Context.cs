9966fcd60d2fdfec7b72fb7d1ed9c41768c23c8a
ElPelfry <delfry_paulino@ucne.edu.do>
2024-11-03 20:10:09 -0400

    Agrega contexto

---
+++ FitGoalAPI/DAL/Context.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shared.Models;
+
+namespace FitGoalAPI.DAL;
+
+public class Context : DbContext
+{
+    public Context(DbContextOptions<Context> options) : base(options) { }
+
+    public DbSet<Usuarios> Usuarios { get; set; }
+    public DbSet<Rutinas> Rutinas { get; set; }
+    public DbSet<RutinaEjercicios> RutinaEjercicios { get; set; }
+    public DbSet<Ejercicios> Ejercicios { get; set; }
+    public DbSet<Entrenamientos> Entrenamientos { get; set; }
+    public DbSet<ProgresoUsuarios> ProgresoUsuarios { get; set; }
+    public DbSet<Tips> Tips { get; set; }
+}
