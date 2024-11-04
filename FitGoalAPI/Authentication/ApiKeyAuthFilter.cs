e8d40585fd64e9e9b8f6ccc5e3eaa11ad7c27c19
ElPelfry <delfry_paulino@ucne.edu.do>
2024-11-03 21:07:12 -0400

    Agrega los filtros de Auth del ApiKey

---
+++ FitGoalAPI/Authentication/ApiKeyAuthFilter.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Configuration;
+
+namespace FitGoalAPI.Authentication;
+
+public class ApiKeyAuthFilter(IConfiguration _configuration) : IAuthorizationFilter
+{
+    public void OnAuthorization(AuthorizationFilterContext context)
+    {
+        if (!context.HttpContext.Request.Headers.TryGetValue(AuthConstants.ApiKeyHeaderName, 
+            out var apiKey))
+        {
+            context.Result = new UnauthorizedObjectResult("API Key is missing");
+            return;
+        }
+
+        var apiKeyValue = _configuration.GetValue<string>(AuthConstants.ApiKeySectionName);
+        if (!apiKey.Equals(apiKeyValue))
+        {
+            context.Result = new UnauthorizedObjectResult("Invalid API Key");
+            return;
+        }
+    }
+}
