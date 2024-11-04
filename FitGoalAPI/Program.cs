5e70c53d477479b52cdeb978023ab71ad28719a4
ElPelfry <delfry_paulino@ucne.edu.do>
2024-11-03 21:08:30 -0400

    Agrega la Autenticacion de Swagger e inyecta el filtro

--- FitGoalAPI/Program.cs
+++ FitGoalAPI/Program.cs
@@ -1,6 +1,8 @@
+using FitGoalAPI.Authentication;
 using FitGoalAPI.DAL;
 using FitGoalAPI.Service;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using Shared.Abstractions;
 using Shared.Models;
 
@@ -8,7 +10,34 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.AddSecurityDefinition("ApiKey", new OpenApiSecurityScheme
+    {
+        Name = "X-Api-Key",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Description = "The API KEY to have Api Access",
+        Scheme = "ApiKeyScheme"
+    });
+
+    var scheme = new OpenApiSecurityScheme
+    {
+        Reference = new OpenApiReference
+        {
+            Type = ReferenceType.SecurityScheme,
+            Id = "ApiKey"
+        },
+        In = ParameterLocation.Header,
+    };
+
+    var requirement = new OpenApiSecurityRequirement
+    {
+        { scheme, new List<string>() }
+    };
+
+    c.AddSecurityRequirement(requirement);
+});
 
 var ConStr = builder.Configuration.GetConnectionString("ConStr");
 
@@ -21,6 +50,8 @@ builder.Services.AddScoped<IUserService<ProgresoUsuarios>, ProgresoUsuariosServi
 builder.Services.AddScoped<IUserService<Rutinas>, RutinasService>();
 builder.Services.AddScoped<IGenericService<Ejercicios>, EjerciciosService>();
 builder.Services.AddScoped<IGenericService<Tips>, TipsService>();
+
+builder.Services.AddScoped<ApiKeyAuthFilter>();
 
 var app = builder.Build();
 
