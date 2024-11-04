c1bef952773028b4da264e859acdb1bef27345f8
ElPelfry <delfry_paulino@ucne.edu.do>
2024-11-03 20:08:56 -0400

    Agrega Interfaces necesarias para los Servicios

---
+++ Shared/Abstractions/IGenericService.cs
@@ -0,0 +1,10 @@
+﻿namespace Shared.Abstractions;
+
+public interface IGenericService<T>
+{   
+    Task<T> Add(T type);
+    Task<T> Get(int id);
+    Task<bool> Update(T type);
+    Task<bool> Delete(int id);
+    Task<List<T>> GetAll();
+}
