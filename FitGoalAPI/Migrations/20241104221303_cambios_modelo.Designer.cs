449f93790d8ea5e0b6b918e86b12ccf336885687
Steven Yesandy Candelario Fernández <steven_candelario@ucne.edu.do>
2024-11-04 18:23:23 -0400

    Modifica columna DuracionTotal en Entrenamientos: de TimeSpan a int

--- FitGoalAPI/Migrations/20241104221303_cambios_modelo.Designer.cs
+++ FitGoalAPI/Migrations/20241104221303_cambios_modelo.Designer.cs
@@ -4,6 +4,7 @@ using FitGoalAPI.DAL;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace FitGoalAPI.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20241104221303_cambios_modelo")]
+    partial class cambios_modelo
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
