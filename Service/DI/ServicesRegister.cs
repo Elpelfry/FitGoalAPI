ddb2e6af3dd092488a18b529d1c5992647aed28d
ElPelfry <delfry_paulino@ucne.edu.do>
2024-11-29 04:01:15 -0400

    Agrega Di de los Services

---
+++ Service/DI/ServicesRegister.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Abstractions;
+using Data.DI;
+using Microsoft.Extensions.Configuration;
+
+namespace Service.DI;
+
+public static class ServicesRegister
+{
+    public static IServiceCollection RegisterServices(this IServiceCollection services)
+    {
+        services.AddScoped<IEjercicioService, EjerciciosService>();
+        services.AddScoped<IEntrenamientoService, EntrenamientosService>();
+        services.AddScoped<IHorarioBebidaService, HorarioBebidasService>();
+        services.AddScoped<IProgresoUsuarioService, ProgresoUsuariosService>();
+        services.AddScoped<IRutinaService, RutinasService>();
+        services.AddScoped<ITipService, TipsService>();
+        services.AddScoped<IUsuarioService, UsuariosService>();
+        return services;
+    }
+}
