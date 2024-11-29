5e06321fdf4c924a92316ac31143b146baeaedbc
ElPelfry <delfry_paulino@ucne.edu.do>
2024-11-29 03:59:40 -0400

    Se agrega las obstracciones de cada entidad

---
+++ Abstractions/IRutinaService.cs
@@ -0,0 +1,12 @@
+﻿using Domain.DTO;
+
+namespace Abstractions;
+
+public interface IRutinaService
+{
+    Task<RutinasDto> Add(RutinasDto rutina);
+    Task<RutinasDto> Get(int id);
+    Task<bool> Update(RutinasDto rutina);
+    Task<bool> Delete(int id);
+    Task<List<RutinasDto>> GetListByUID(string UID);
+}
---
+++ Abstractions/IUsuarioService.cs
@@ -0,0 +1,11 @@
+﻿using Domain.DTO;
+
+namespace Abstractions;
+
+public interface IUsuarioService
+{
+    Task<UsuariosDto> Add(UsuariosDto usuario);
+    Task<UsuariosDto> Get(string id);
+    Task<bool> Update(UsuariosDto usuario);
+    Task<bool> Delete(string id);
+}
