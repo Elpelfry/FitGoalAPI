1c932379e29f84b4c3fc8beb3b41c6b6ebca3c02
ElPelfry <delfry_paulino@ucne.edu.do>
2024-11-29 03:58:08 -0400

    Se agrega los modelos

---
+++ Data/Models/RutinaEjercicios.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Data.Models;
+
+public class RutinaEjercicios
+{
+    [Key]
+    public int RutinaEjercicioId { get; set; }
+    [ForeignKey("Rutinas")]
+    public int RutinaId { get; set; }
+    [ForeignKey("Ejercicios")]
+    public int EjercicioId { get; set; }
+    public int Repeticiones { get; set; }
+    public int Series { get; set; }
+    public int Descanso { get; set; }
+}
