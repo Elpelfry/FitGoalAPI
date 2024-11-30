37109d7825af0706bfec4089c0d6294063d0509f
ElPelfry <delfry_paulino@ucne.edu.do>
2024-11-30 17:24:06 -0400

    agrega Peso Inicial

--- Domain/DTO/UsuariosDto.cs
+++ Domain/DTO/UsuariosDto.cs
@@ -8,6 +8,7 @@ public class UsuariosDto
     public string? Correo { get; set; }
     public int Edad { get; set; }
     public float Altura { get; set; }
+    public float PesoInicial { get; set; }
     public float PesoActual { get; set; }
     public float PesoIdeal { get; set; }
     public float AguaDiaria { get; set; }
